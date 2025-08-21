d45edab3f476b171a4af4c590b8baa12fa762cfc
carri <carriondaniel602@gmail.com>
2025-08-21 05:12:23 -0500

    feat(Language Selection Bug Fix_2): Implemented lenguage selection bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
     site: 'https://DanproTDT.github.io',
     output: 'static', // Esto es clave para habilitar endpoints dinámicos
-    adapter: node({ mode: 'standalone' }),
     integrations: [tailwind()]
 });
 
